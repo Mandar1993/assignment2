8fddcb802bd462ad9508d0bebc73382bd293ca14
Mandar1993 <60645059+Mandar1993@users.noreply.github.com>
2020-08-09 19:40:36 +0530

    Update 2.py
    output:
    * 
    ** 
    *** 
    **** 
    *****
    ****
    ***
    **
    *

--- 2.py
+++ 2.py
@@ -1,13 +1,10 @@
 rows = 5
 for num in range(rows):
     for i in range(num):
-        print("*", end=" ")  # print number
-     #line after each row to display pattern correctly
+        print("*", end="")  
     print(" ")
-rowss = 5
-b = 0
+rowss=5
 for i in range(rowss, 0, -1):
-    b += 1
-    for j in range(1, i + 1):
-        print("*", end=' ')
-    print('\r')
+    for j in range(0,i):
+        print("*", end="")
+    print()
